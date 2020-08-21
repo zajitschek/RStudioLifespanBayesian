e4dff50e7239b971ba7e8aed801652f08b2c84d6
zajitschek <64367116+zajitschek@users.noreply.github.com>
2020-08-21 21:04:08 +0100

    Update rstanarm_surv.R

--- R/rstanarm_surv.R
+++ R/rstanarm_surv.R
@@ -10,12 +10,13 @@
 suppressMessages(library(dplyr))
 suppressMessages(library(rstanarm))
 data1 <- read.csv("https://github.com/ZajitschekTeam/lifespananalysis/raw/master/binder/data/expevol_male_flies.csv")
-data1 <- data1 %>% mutate(across(where(is.integer), as.factor))
-          
+data1 <- data1 %>% mutate(across(where(is.integer), as.factor), status = 1)  # deaths were observed for all individuals
+
 # Your first Bayesian Cox PH model
 #  For now, we will assume that Gaussian errors are ok (this is the default)
 
-stan_surv_model1 <- stan_surv(lifespan ~ assaydiet + (1|vial), data= data1, seed= 111)
+stan_surv_model1 <- stan_surv(Surv(lifespan, status) ~ assaydiet + (1|vial), data= data1, seed= 111)
+
 plot(stan_surv_model1)
 summary(stan_surv_model1)
 
